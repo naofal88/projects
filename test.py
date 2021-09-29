630dc6f9e83c2fe41a47458844071da8d1d5a6e3
Naofal EL Ftouh <naofal.elftouh@routz.nl>
2021-09-29 08:27:27 +0200

    test

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
+age =input("How old are you?")
+height = input("How much do you weight?")
+weight = input("How much do you weight?")
+print(f"So, you're {age} old, {height} tall and {weight} heavy")
+
+
+
 print("How old are you?", end='>>>')
 age = input()
 print("How tall are you", end='>>>')
