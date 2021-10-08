b274b8f011af422d3f5192d69f626f0bf92fdd92
Naofal EL Ftouh <naofal.elftouh@routz.nl>
2021-10-08 08:41:40 +0200

    ex 16

--- ex18.py
+++ ex18.py
@@ -2,20 +2,19 @@
 def print_two(*args):
     arg1, arg2 = args
     print(f"arg1: {arg1}, arg2: {arg2}")
-    #ok, that *args is actually pointless, we can just do this
+#ok, that *args is actually pointless, we can just do this
 
-    def print_two_again(arg1, arg2):
-        print(f"arg1: {arg1}, arg2: {arg2}")
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
 
-        #this just take one argument
-        def print_one(arg1):
-            print(f"arg1: {arg1}")
-            #this one takes no arguments
-            def print_none():
-                print("I got nothing'.")
-
-                print_two("Naofal", "EL Ftouh")
-                print_two_again("Naofal", "EL Ftouh")
-                print_one("First!")
-                print_none()
+#this just take one argument
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+#this one takes no arguments
+def print_none():
+    print("I got nothing'.")
+    print_two("Naofal", "Ftouh")
+    print_two_again("Naofal", "Ftouh")
+    print_one("First!")
+    print_none()
 
