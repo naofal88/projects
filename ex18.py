099bc663583f58fb5bb484c8930b25300c1db028
Naofal EL Ftouh <naofal.elftouh@routz.nl>
2021-10-08 09:10:28 +0200

    ex 18

--- ex18.py
+++ ex18.py
@@ -1,20 +1,23 @@
 #This one is like your scripts with argv
 def print_two(*args):
-    arg1, arg2 = args
-    print(f"arg1: {arg1}, arg2: {arg2}")
+    arg1, arg2, arg3 = args
+    print(f"arg1: {arg1}, arg2: {arg2},{arg3}")
 #ok, that *args is actually pointless, we can just do this
 
-def print_two_again(arg1, arg2):
-    print(f"arg1: {arg1}, arg2: {arg2}")
+def print_two_again(arg1, arg2, arg3):
+    print(f"arg1: {arg1}, arg2: {arg2},{arg3}")
 
 #this just take one argument
-def print_one(arg1):
-    print(f"arg1: {arg1}")
+def print_one(arg1, arg2):
+    print(f"arg1: {arg1}, {arg2}")
 #this one takes no arguments
+def test(abc1, abc2):
+    print(f"I got nothing'.", f"Cerrar, {abc1}, {abc2}")
 def print_none():
-    print("I got nothing'.")
-print_two("Naofal", "Ftouh")
-print_two_again("Naofal", "Ftouh")
-print_one("First!")
+    print("Cerramos")
+print_two("Naofal", "Ftouh" , "SAid")
+print_two_again("Naofal", "Ftouh", "Said")
+print_one("First!", "Adios")
+test("NAda", "de nada")
 print_none()
 
